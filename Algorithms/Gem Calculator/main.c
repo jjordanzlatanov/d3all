c551803b706f92013ba2f2932f317a4cf1e762a0
jordanzz <jjordanzlatanov@gmail.com>
2021-08-02 02:23:46 +0300

    Added base variables for gems

--- Algorithms/Gem Calculator/main.c
+++ Algorithms/Gem Calculator/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    int white[2] = {15, 10};
-    int purple[2] = {10, 10};
-    int green[2] = {10, 10};
-    int red[2] = {10, 10};
-    int yellow[2] = {10, 10};
-    
-    int firstconvertions = white[0] / 9 + white[1] / 9;
+    int amount_gems = 1; // How many gems you want?
 
-    white[0] = white[0] - (white[0] / 9) * 9;
-    white[1] = white[1] - (white[1] / 9) * 9;
+    int gem_tier = 1; //  0 - Marquise; 1 - Imperial; 2 - Flawless Imperial; 3 - Royal; 4 - Flawless Royal
+
+    int gem_type = 1; //  0 - Topaz; 1 - Ruby; 2 - Emerald; 3 - Amethyst; 4 - Diamond
     
-    printf("%d", white[0]); 
+    int gems[5][2] = { {10, 10}, {20, 20}, {30, 30}, {40, 40}, {50, 50} }; // Input your amount of marquise and imperial gems
+
+    int gem_sum[5][5] = { {gems[0][0], gems[0][1], 0, 0, 0},  {gems[1][0], gems[1][1], 0, 0, 0}, {gems[2][0], gems[2][1], 0, 0, 0}, {gems[3][0], gems[3][1], 0, 0, 0}, {gems[4][0], gems[4][1], 0, 0, 0} };
+
+    int essence_amount[5];
+
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if(gem_type != j){
+                gem_sum[gem_type][gem_tier] += (gem_sum[gem_type][j] / 9) * 9;
+            }
+        }
+    }
+    
+    
 
 
     return 0;
