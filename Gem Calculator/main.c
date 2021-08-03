83a217e33ecea1d22cf70dc39e3d743772ab498a
jordanzz <jjordanzlatanov@gmail.com>
2021-08-03 10:00:10 +0300

    Optimizing gem calculator to only use the ones we need

--- Gem Calculator/main.c
+++ Gem Calculator/main.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    int amount_gems = 1; // How many gems you want?
+    int amount_gems = 3; // How many gems you want?
 
-    int gem_tier = 4; //  0 - Marquise; 1 - Imperial; 2 - Flawless Imperial; 3 - Royal; 4 - Flawless Royal
+    int gem_tier = 3; //  0 - Marquise; 1 - Imperial; 2 - Flawless Imperial; 3 - Royal; 4 - Flawless Royal
+
+    int gems_needed[5];
 
     int gem_type = 2; //  0 - Topaz; 1 - Ruby; 2 - Emerald; 3 - Amethyst; 4 - Diamond
-    
+
     int gems[5][2] = { {10, 10}, {20, 20}, {30, 30}, {40, 40}, {50, 50} }; // Input your amount of marquise and imperial gems
 
     int gem_sum[5];
 
-    for(int i = 0; i < 5; i++){ // i -  Gem tier
-        if(i < 2){
-            for(int j = 0; j < 5; j++){ // j - Gem type
-                if(j != gem_type){
-                    gem_sum[i] += (gems[j][i] / 9) * 9;
-                }else{
-                    gem_sum[i] += gems[j][i];
-                }
-            }
-        }
+    gems_needed[gem_tier] = amount_gems;
 
-        if(i > 0){
-            gem_sum[i] += (gem_sum[i - 1] / 3) * 3;
-        }
-
-        if(i == gem_tier){
-            printf("%d", gem_sum[i]);
-        }
+    for(int i = gem_tier - 1; i >= 0; i--){
+        gems_needed[i] = gems_needed[i + 1] * 3;
     }
+
+    
 
     return 0;
 }
